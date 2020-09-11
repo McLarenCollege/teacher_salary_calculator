3a127428b9b48aee52213365135cfe2bb3f6c132
McLarenCollege <hello@mclarencollege.in>
2020-09-11 15:54:21 +0530

    Updates

---
+++ precache-manifest.a3f08ee18be3f459782a5bae7b266c1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2eab0412d64c521b727244aabdc09ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24906c898e6668ffc633",
+    "url": "/static/css/main.be0efba1.chunk.css"
+  },
+  {
+    "revision": "4dbece6b24e65dd916c7",
+    "url": "/static/js/2.c9914fab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9914fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24906c898e6668ffc633",
+    "url": "/static/js/main.0496ed4c.chunk.js"
+  },
+  {
+    "revision": "9e186d3adc0e3f69c7ca",
+    "url": "/static/js/runtime-main.4dbe596c.js"
+  },
+  {
+    "revision": "ef19a4efc4a6ebd9876086fa4898edf7",
+    "url": "/static/media/logo-white.ef19a4ef.png"
+  }
+]);
